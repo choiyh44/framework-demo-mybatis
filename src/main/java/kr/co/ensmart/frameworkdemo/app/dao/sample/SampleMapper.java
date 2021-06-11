853b17a493fd1dc570de976427f7dc1f51cdde8a
choiyh <choiyh44@gmail.com>
2021-06-11 17:18:10 +0900

    mybatis 로깅 추가

--- src/main/java/kr/co/ensmart/frameworkdemo/app/dao/sample/SampleMapper.java
+++ src/main/java/kr/co/ensmart/frameworkdemo/app/dao/sample/SampleMapper.java
@@ -9,4 +9,7 @@ import kr.co.ensmart.frameworkdemo.common.dto.sample.Sample;
 @Repository
 public interface SampleMapper {
 	List<Sample> selectAllSampleList();
+	
+	Sample selectSampleById(Integer id);
+
 }
